baa779c1ca80cd679d78ec28b1f272e2e7bdeb5d
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-09 01:10:44 +0800

    test(*): use sinon for stubbing

--- test/ckip_client.test.js
+++ test/ckip_client.test.js
@@ -1,79 +1,100 @@
 'use strict'
 
+var fs = require('fs')
+var path = require('path')
 var expect = require('chai').expect
+var sinon = require('sinon')
 var ckip = require('../')('0.0.0.0', 0, 'username', 'password')
 
+const mockResponse = fs.readFileSync(path.resolve(__dirname, 'mock.xml'), 'utf-8')
+
 describe('CKIPClient', function () {
-  var error, response
+  var sandbox
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create()
+  })
+
+  afterEach(function () {
+    sandbox.restore()
+  })
 
   describe('#request()', function () {
-    var promise
-
-    before(function (done) {
-      promise = ckip.request('台新金控12月3日將召開股東臨時會進行董監改選。', function (err, res) {
-        error = err
-        response = res
-        done()
-      })
-    })
+    var text = '台新金控12月3日將召開股東臨時會進行董監改選。'
 
     it('should get response without error if initial parameters are valid', function (done) {
-      if (error) return done()
-      expect(response).to.exist
-      done()
-    })
+      sandbox.stub(ckip, 'request').withArgs(text).yields(null, mockResponse)
 
-    it('should return promise with response', function (done) {
-      if (error) return done()
-      promise.then(function (res) {
-        expect(res).to.exist
+      ckip.request(text, function (err, res) {
+        expect(err).to.not.exist
+        expect(res).to.equal(mockResponse)
         done()
       })
+    })
+
+    it('should return promise with response', function () {
+      sandbox.stub(ckip, 'request').withArgs(text).returns(Promise.resolve(mockResponse))
+
+      return ckip.request(text)
+        .then(function (res) {
+          expect(res).to.equal(mockResponse)
+        })
     })
   })
 
   describe('#getSentences()', function () {
-    var promise
+    var expected = ['　台新(N)　金控(N)　12月(N)　3日(N)　將(ADV)　召開(Vt)　股東(N)　臨時會(N)　進行(Vt)　董監(N)　改選(Nv)　。(PERIODCATEGORY)']
 
     it('should get setences without error', function (done) {
-      if (error) return done()
-      promise = ckip.getSentences(response, function (err, setences) {
+      ckip.getSentences(mockResponse, function (err, setences) {
         expect(err).to.not.exist
         expect(setences).to.exist
-        expect(setences).to.be.instanceof(Array)
+        expect(setences).to.be.an('array')
+        expect(setences).to.be.eql(expected)
         done()
       })
     })
 
-    it('should return promise with setences', function (done) {
-      if (error) return done()
-      promise.then(function (setences) {
+    it('should return promise with setences', function () {
+      return ckip.getSentences(mockResponse).then(function (setences) {
         expect(setences).to.exist
-        expect(setences).to.be.instanceof(Array)
-        done()
+        expect(setences).to.be.an('array')
+        expect(setences).to.be.eql(expected)
       })
     })
   })
 
   describe('#getTerms()', function () {
-    var promise
+    var expected = [
+      { term: '台新', tag: 'N' },
+      { term: '金控', tag: 'N' },
+      { term: '12月', tag: 'N' },
+      { term: '3日', tag: 'N' },
+      { term: '將', tag: 'ADV' },
+      { term: '召開', tag: 'Vt' },
+      { term: '股東', tag: 'N' },
+      { term: '臨時會', tag: 'N' },
+      { term: '進行', tag: 'Vt' },
+      { term: '董監', tag: 'N' },
+      { term: '改選', tag: 'Nv' },
+      { term: '。', tag: 'PERIODCATEGORY' }
+    ]
 
     it('should get terms without error', function (done) {
-      if (error) return done()
-      promise = ckip.getTerms(response, function (err, terms) {
+      ckip.getTerms(mockResponse, function (err, terms) {
         expect(err).to.not.exist
         expect(terms).to.exist
-        expect(terms).to.be.instanceof(Array)
+        expect(terms).to.be.an('array')
+        expect(terms).to.be.eql(expected)
         done()
       })
     })
 
-    it('should return promise with terms', function (done) {
-      if (error) return done()
-      promise.then(function (terms) {
+    it('should return promise with terms', function () {
+      return ckip.getTerms(mockResponse).then(function (terms) {
         expect(terms).to.exist
-        expect(terms).to.be.instanceof(Array)
-        done()
+        expect(terms).to.be.an('array')
+        expect(terms).to.be.eql(expected)
       })
     })
   })
